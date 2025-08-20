3a62907333b67f72941fc0de6bebfa64f38f9be3
osad-sakana <mezzoforte.mackerel@icloud.com>
2025-08-20 12:29:30 +0900

    Hello World APIを作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::{OpenApi, OpenApiService, payload::PlainText};
+
+struct Api;
+
+#[OpenApi]
+impl Api{
+    #[oai(path = "/hello", method = "get")]
+    async fn hello(&self) -> PlainText<String> {
+        PlainText("Hello, world!".to_string())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let api_service = OpenApiService::new(Api, "Hello API", "1.0.0");
+    let ui = api_service.swagger_ui();
+    let app = Route::new()
+        .nest("/", api_service)
+        .nest("/docs", ui);
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
 }
